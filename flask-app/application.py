7229048cb426badaddbe4889552c5185960b3902
shajal <shajal@shajals-MacBook-Pro.local>
2018-08-13 10:26:01 +0600

    Retailer,Wholesaler,factory page is working

--- flask-app/application.py
+++ flask-app/application.py
@@ -10,7 +10,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/factory")
+def factory():
+    transactions = {
+		"medicine": {"name": "med123", "date": "29/06/1996"}
+	}
+    return render_template('factory.html', title="Factory", transactions=transactions)
 
+
+@app.route("/wholesaler")
+def wholesaler():
+    return render_template('wholesaler.html',title="Wholesaler")
+
+
+@app.route("/retailer")
+def retailer():
+    return render_template('retailer.html', title="Retailer")
+
+@app.route("/customer")
+def customer():
+    return render_template('customer.html',title="Customer")
 
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 5000))
